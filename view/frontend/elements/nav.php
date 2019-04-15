5238b51943b21082859c73d2290118669c32fd66
nico-kercode <nicodevweb@gmail.com>
2019-04-15 18:01:07 +0200

    modif  intro membre rgpd register

--- view/frontend/elements/nav.php
+++ view/frontend/elements/nav.php
@@ -83,106 +83,108 @@
                         </button>
                     </div>
                     <div class="modal-body">
-                        <div class="col-sm-12" >
-                            <h6>Pas encore enregistré ?  </h6>
-                            <a class="btn btn-warning" href="index.php?action=formRegister" >S'enregistrer</a>
-               
-                </div>        
-                            </div>
-                            
+                        <div class="col-sm-12">
+                            <h6>Pas encore enregistré ? </h6>
+                            <a class="btn btn-warning" href="index.php?action=formRegister">S'enregistrer</a>
+
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
-        <div class=" row">
-                                    <div class="col-sm-8 offset-sm-2 my-4">
-                                    </div>
-                        </div>
-                        <div class="row ">
-                            <!-- BARRE DE RECHERCHE  -->
-                            <div class="col-sm-8 offset-sm-3 d-flex ">
+    </div>
+    <div class=" row">
+        <div class="col-sm-8 offset-sm-2 my-4">
+        </div>
+    </div>
+    <div class="row ">
+        <!-- BARRE DE RECHERCHE  -->
+        <div class="col-sm-8 offset-sm-3 d-flex ">
 
-                                <?php if(isset($_SESSION['rang']) && $_SESSION['rang'] == '2'):?>
-                                <h6 class="bienvenue"> <em> Bienvenue chez toi
-                                        <span><?=  ucfirst($_SESSION['pseudo'])?></span>. Il y a
-                                        actuellement <?= $total ?> activités référencées, un total de
-                                        <?= $totalMembres ?> membres enregistrés. Nos membres nous on signalés
-                                        <?=$nbAlert['nbreAlerts']?>
-                                        messages </em>
-                                </h6>
-                                <?php endif; ?>
+            <?php if(isset($_SESSION['rang']) && $_SESSION['rang'] == '2'):?>
+            <h6 class="bienvenue"> <em> Bienvenue chez toi
+                    <span><?=  ucfirst($_SESSION['pseudo'])?></span>. Il y a
+                    actuellement <?= $total ?> activités référencées, un total de
+                    <?= $totalMembres ?> membres enregistrés. Nos membres nous on signalés
+                    <?=$nbAlert['nbreAlerts']?>
+                    messages </em>
+            </h6>
+            <?php endif; ?>
 
-                                <?php if(isset($_SESSION['rang']) && $_SESSION['rang'] == '0' || $_SESSION['rang'] =='1'):?>
-                                <h6 class="bienvenue"> <em> Bienvenue <span><?=  ucfirst($_SESSION['pseudo'])?></span>,
-                                        merci de vous
-                                        être
-                                        connecté(e). Il y a actuellement <?= $total ?> activités référencées et un total
-                                        de
-                                        <?= $totalMembres ?> membres enregistrés . </em></h6>
-                                <?php endif; ?>
+            <?php if(isset($_SESSION['rang']) && $_SESSION['rang'] == '0' || $_SESSION['rang'] =='1'):?>
+            <h6 class="bienvenue"> <em> Bienvenue <span><?=  ucfirst($_SESSION['pseudo'])?></span>,
+                    merci de vous
+                    être
+                    connecté(e). Il y a actuellement <?= $total ?> activités référencées et un total
+                    de
+                    <?= $totalMembres ?> membres enregistrés . </em></h6>
+            <?php endif; ?>
 
-                                <?php if(empty($_SESSION)):?>
-                                <h6 class="bienvenue"> <em> Pour plus de contenu :</em> 
-                                <a href="index.php?action=formRegister">Enregistrez vous !</a>
-                                       
-                                        
+            <div class="row <?= $_GET['action'] == 'rgpd' 
+    || $_GET['action'] == 'moncompte'
+    || $_GET['action'] == 'formRegister'
+     ? 'none' : '' ?>">
+                <?php if(empty($_SESSION)):?>
+                <h6 class="bienvenue"> <em> Pour plus de contenu :</em>
+                    <a href="index.php?action=formRegister">Enregistrez vous !</a>
 
 
-                                    </a> <em> &nbsp; Il y a actuellement <?= $total ?> activités référencées et un total
-                                        de
-                                        <?= $totalMembres ?> membres enregistrés .</em> </h6>
-                                <?php endif; ?>
-                            </div>
-                        </div>
 
-                        <div class="row <?= $_GET['action'] == 'rgpd' 
+
+                    </a> <em> &nbsp; Il y a actuellement <?= $total ?> activités référencées et un total
+                        de
+                        <?= $totalMembres ?> membres enregistrés .</em> </h6>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <div class="row <?= $_GET['action'] == 'rgpd' 
     || $_GET['action'] == 'moncompte'
     || $_GET['action'] == 'formRegister'
     || $_GET['action'] == 'ajoutAnnonce' ? 'none' : '' ?>">
 
-                            <div class="col-sm-6 offset-sm-3 border-dark ">
-                                <form method="POST" action="index.php?action=search" class="input-group  input-lg my-2">
-                                    <input class=" form-control border-dark  " type="search" value="" name="searchbar"
-                                        id="search" required>
-                                    <span class="input-group-append">
-                                        <button class="btn border-left-2 border border-dark " name="submitSearch"
-                                            type="submit">
-                                            <i class="fa fa-search"></i>
-                                        </button>
-                                    </span>
+        <div class="col-sm-6 offset-sm-3 border-dark ">
+            <form method="POST" action="index.php?action=search" class="input-group  input-lg my-2">
+                <input class=" form-control border-dark  " type="search" value="" name="searchbar" id="search" required>
+                <span class="input-group-append">
+                    <button class="btn border-left-2 border border-dark " name="submitSearch" type="submit">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </span>
 
-                                </form>
-                            </div>
-                        </div> <!-- row -->
+            </form>
+        </div>
+    </div> <!-- row -->
 
-                        <div class="row <?= $_GET['action'] == 'rgpd' 
+    <div class="row <?= $_GET['action'] == 'rgpd' 
     || $_GET['action'] == 'moncompte'
     || $_GET['action'] == 'formRegister'
     || $_GET['action'] == 'ajoutAnnonce' ? 'none' : '' ?>">
 
-                            <div class="d-inline-flex col-sm-12 col-md-10 offset-md-2" id="choixAccueil">
-                                <div class="row">
-                                    <div class="col-sm-12 col-md-6 mx-auto ">
-                                        <a class="btn btn-warning  border-dark   <?= $_GET['action'] == 'vannes' ? 'active' : '' ?>"
-                                            href="index.php?action=vannes&page=1">Secteur de Vannes</a>
+        <div class="d-inline-flex col-sm-12 col-md-10 offset-md-2" id="choixAccueil">
+            <div class="row">
+                <div class="col-sm-12 col-md-6 mx-auto ">
+                    <a class="btn btn-warning  border-dark   <?= $_GET['action'] == 'vannes' ? 'active' : '' ?>"
+                        href="index.php?action=vannes&page=1">Secteur de Vannes</a>
 
-                                        <a class="btn btn-warning border-dark   <?= $_GET['action'] == 'lorient' ? 'active' : '' ?>"
-                                            href="index.php?action=lorient&page=1">Secteur de Lorient</a>
+                    <a class="btn btn-warning border-dark   <?= $_GET['action'] == 'lorient' ? 'active' : '' ?>"
+                        href="index.php?action=lorient&page=1">Secteur de Lorient</a>
 
-                                        <a class="btn btn-warning border-dark <?= $_GET['action'] == 'meilleurNote' ? 'active' : '' ?>"
-                                            href="index.php?action=meilleurNote">Les 10 mieux notés</a>
+                    <a class="btn btn-warning border-dark <?= $_GET['action'] == 'meilleurNote' ? 'active' : '' ?>"
+                        href="index.php?action=meilleurNote">Les 10 mieux notés</a>
 
-                                        <?php if(!empty($getSelection)):?>
+                    <?php if(!empty($getSelection)):?>
 
-                                        <a class="btn btn-warning  border-dark  <?= $_GET['action'] == 'monPanier' ? 'active' : '' ?>"
-                                            href="index.php?action=monPanier&id_MEMBRES=<?= $_SESSION['id']?>">Ma
-                                            selection</a>
-                                    </div>
-                                </div>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </div>
-                    <!-- important ne pas suppr les 2 </div> (include) -->
+                    <a class="btn btn-warning  border-dark  <?= $_GET['action'] == 'monPanier' ? 'active' : '' ?>"
+                        href="index.php?action=monPanier&id_MEMBRES=<?= $_SESSION['id']?>">Ma
+                        selection</a>
                 </div>
             </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+<!-- important ne pas suppr les 2 </div> (include) -->
+</div>
+</div>
